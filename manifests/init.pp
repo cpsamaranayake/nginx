f411ff1558dd212ac37646f8db85df4eb358dad6
root <root@binddns.puppetlearning.local>
2016-07-05 04:51:46 -0400

    Code simplication

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,20 @@
 class nginx {
+  File {
+	owner => 'root',
+	group => 'root',
+	mode => '0644',
+	}
+
   package { 'nginx':
 	ensure => 'installed',
 	}
 
   file { '/var/www':
 	ensure => 'directory',
-	owner => 'root',
-	group => 'root',
-	mode => '0775',
 	}
 
   file { '/var/www/index.html':
 	ensure => 'file',
-	owner => 'root',
-	group => 'root',
-	mode => '0644',
 	source => 'puppet:///modules/nginx/index.html',
 	}
 
@@ -25,9 +25,6 @@ class nginx {
 
   file { '/etc/nginx/nginx.conf':
 	ensure => 'file',
-	owner => 'root',
-	group => 'root',
-	mode => '0644',
 	source => 'puppet:///modules/nginx/nginx.conf',
 	notify => Service['nginx'],
 	require => Package['nginx'],
@@ -35,9 +32,6 @@ class nginx {
 
   file { '/etc/nginx/conf.d/default.conf':
 	ensure => 'file',
-	owner => 'root',
-	group => 'root',
-	mode => '0644',
 	source => 'puppet:///modules/nginx/default.conf',
 	notify => Service['nginx'],
 	require => Package['nginx'],
