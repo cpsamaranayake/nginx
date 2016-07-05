7d3a3d7b6d385006377e9a2c365cfc85f9d540c4
root <root@binddns.puppetlearning.local>
2016-07-05 04:27:19 -0400

    added nginx.conf & default.conf files

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,4 +17,29 @@ class nginx {
 	mode => '0644',
 	source => 'puppet:///modules/nginx/index.html',
 	}
+
+  service { 'nginx':
+	ensure => 'running',
+	enable => 'true',
+	}
+
+  file { '/etc/nginx/nginx.conf':
+	ensure => 'file',
+	owner => 'root',
+	group => 'root',
+	mode => '0644',
+	source => 'puppet:///modules/nginx/nginx.conf',
+	notify => Service['nginx'],
+	require => Package['nginx'],
+	}
+
+  file { '/etc/nginx/conf.d/default.conf':
+	ensure => 'file',
+	owner => 'root',
+	group => 'root',
+	mode => '0644',
+	source => 'puppet:///modules/nginx/default.conf',
+	notify => Service['nginx'],
+	require => Package['nginx'],
+	}
 }
